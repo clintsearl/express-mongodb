6cf39d7d10ec490004cb04f278392adad2170bac
clintsearl <clintsearl@gmail.com>
2019-04-09 14:09:41 -0600

    CRD but no U yet

--- server.js
+++ server.js
@@ -3,16 +3,15 @@ const app = express()
 const port = 4000
 const bodyParser = require("body-parser")
 const monk = require('monk')
-const MongoClient = require("mongodb").MongoClient
-const ObjectId = require("mongodb").ObjectID
-//All of the instances within the url it is called gearList that is the name of the database 
- 
+// const MongoClient = require("mongodb").MongoClient
+// const ObjectId = require("mongodb").ObjectID
+//All of the instances within the url it is called gearList that is the name of the database cluster
 const url = 'mongodb://clintse:clintspassword@gearlist-shard-00-00-nroed.mongodb.net:27017,gearlist-shard-00-01-nroed.mongodb.net:27017,gearlist-shard-00-02-nroed.mongodb.net:27017/gearList?ssl=true&replicaSet=gearList-shard-0&authSource=admin&retryWrites=true'
 const db = monk(url);
-
 db.then(() => {
   console.log('Connected correctly to server')
 })
+
 //gear is what monk is getting, the name of the document it's on the line below
 const collection = db.get('gear')
 
@@ -25,15 +24,20 @@ app.get('/', async (req, res) => {
 
 app.post('/', async (req, res)=>{
     const result = await collection.insert(req.body)
-    res.status(200).send(result)
+    return res.status(200).send(result)
 
 })
 
 app.delete('/', async (req, res)=>{
     await collection.findOneAndDelete(req.body)
     return res.status(200).send(await collection.find())    
-// }).catch((err)=>{
-//     console.log(err)
+})
+
+app.put('/', async (req, res)=>{
+    const result= await collection.find(headers)
+    .then(collection.findOneAndUpdate(req.body))
+   return res.status(200).send(result)
+
 })
 
 
