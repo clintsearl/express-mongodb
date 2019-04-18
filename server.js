cf3b2e1960f5baa374f2d0fecfcffdb69fe78489
clintsearl <clintsearl@gmail.com>
2019-04-18 11:23:27 -0600

    another commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 4001
+const port = process.env.PORT || 4001;
 const bodyParser = require("body-parser")
 const monk = require('monk')
 const cors = require('cors')
